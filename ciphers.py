33a215da05777048bf749707e4f2aedcb4f1f62f
Skopiuk <skopiuk.sv@phystech.edu>
2021-04-21 18:08:49 +0300

    open -> with open

--- ciphers.py
+++ ciphers.py
@@ -1,9 +1,10 @@
 import os
 import random
+import string
 from collections import Counter
 
-letters_en = "abcdefghijklmnopqrstuvwxyz"
-letters_ru = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+letters_en = string.ascii_lowercase
+letters_ru = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
 
 
 def get_args():
@@ -17,7 +18,7 @@ def get_args():
 def rand_key(seed, l):
     '''Генерация псевдорандомного ключа с помощью сида'''
     random.seed(seed)
-    return ''.join(random.choice("abcdefghijklmnopqrstuvwxyz") for i in range(l))
+    return ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(l))
 
 
 def caesar(args):
@@ -26,28 +27,27 @@ def caesar(args):
     global letters_ru
     len_en = len(letters_en)
     len_ru = len(letters_ru)
-    if args[1].lower() in ["e", "en", "encrypt"]:
+    if args[1].lower() in ['e', 'en', 'encrypt']:
         step = int(args[2])
-    if args[1].lower() in ["d", "de", "decrypt"]:
+    if args[1].lower() in ['d', 'de', 'decrypt']:
         step = -1 * int(args[2])
     try:
         usr_file = open(args[0], 'r')
     except:
-        return "No such file"
-    cipher = open(args[3], "w")
-    for line in usr_file.readlines():
-        new_txt = ""
-        for i in line.lower():
-            if i not in letters_en and i not in letters_ru:
-                new_txt += i
-            elif i in letters_ru:
-                new_txt += letters_ru[(letters_ru.find(i) + step) % len_ru]
-            else:
-                new_txt += letters_en[(letters_en.find(i) + step) % len_en]
-        cipher.write(new_txt)
+        return 'No such file'
+    with open(args[3], 'w') as cipher:
+        for line in usr_file.readlines():
+            new_txt = ''
+            for i in line.lower():
+                if i not in letters_en and i not in letters_ru:
+                    new_txt += i
+                elif i in letters_ru:
+                    new_txt += letters_ru[(letters_ru.find(i) + step) % len_ru]
+                else:
+                    new_txt += letters_en[(letters_en.find(i) + step) % len_en]
+            cipher.write(new_txt)
     usr_file.close()
-    cipher.close()
-    return "Result in " + args[3]
+    return 'Result in ' + args[3]
 
 
 def vigenere(args):
@@ -59,31 +59,32 @@ def vigenere(args):
     if not args[2].isalpha():
         raise TypeError
     len_key = len(args[2])
-    if args[1].lower() in ["e", "en", "encrypt"]:
+    if args[1].lower() in ['e', 'en', 'encrypt']:
         step = 1
-    if args[1].lower() in ["d", "de", "decrypt"]:
+    if args[1].lower() in ['d', 'de', 'decrypt']:
         step = -1
     try:
         usr_file = open(args[0], 'r')
     except:
-        return "No such file"
-    cipher = open(args[3], "w")
-    spase_couner = 0
-    for line in usr_file.readlines():
-        new_txt = ""
-        for j, i in enumerate(line.lower()):
-            if i == ' ':
-                spase_couner += 1
-            if i not in letters_en and i not in letters_ru:
-                new_txt += i
-            elif i in letters_ru:
-                new_txt += letters_ru[(letters_ru.find(i) + step * letters_ru.find(args[2][(j - spase_couner) % len_key])) % len_ru]
-            else:
-                new_txt += letters_en[(letters_en.find(i) + step * letters_en.find(args[2][(j - spase_couner) % len_key])) % len_en]
-        cipher.write(new_txt)
+        return 'No such file'
+    with open(args[3], 'w') as cipher:
+        spase_couner = 0
+        for line in usr_file.readlines():
+            new_txt = ''
+            for j, i in enumerate(line.lower()):
+                if i == ' ':
+                    spase_couner += 1
+                if i not in letters_en and i not in letters_ru:
+                    new_txt += i
+                elif i in letters_ru:
+                    new_txt += letters_ru[
+                        (letters_ru.find(i) + step * letters_ru.find(args[2][(j - spase_couner) % len_key])) % len_ru]
+                else:
+                    new_txt += letters_en[
+                        (letters_en.find(i) + step * letters_en.find(args[2][(j - spase_couner) % len_key])) % len_en]
+            cipher.write(new_txt)
     usr_file.close()
-    cipher.close()
-    return "Result in " + args[3]
+    return 'Result in ' + args[3]
 
 
 def vernam(args):
@@ -92,43 +93,41 @@ def vernam(args):
     global letters_ru
     len_en = len(letters_en)
     len_ru = len(letters_ru)
-    if args[1] in ["e", "en", "encrypt"]:
+    if args[1] in ['e', 'en', 'encrypt']:
         step = 1
-    if args[1] in ["d", "de", "decrypt"]:
+    if args[1] in ['d', 'de', 'decrypt']:
         step = -1
     gen_str = rand_key(args[2], os.stat(args[0]).st_size)
     try:
         usr_file = open(args[0], 'r')
     except:
-        return "No such file"
-    cipher = open(args[3], "w")
-    spase_couner = 0
-    for line in usr_file.readlines():
-        new_txt = ""
-        if i == ' ':
-            spase_couner += 1
-        for j, i in enumerate(line.lower()):
-            if i not in letters_en and i not in letters_ru:
-                new_txt += i
-            elif i in letters_ru:
-                new_txt += letters_ru[(letters_ru.find(i) + step * letters_en.find(gen_str[j - spase_couner])) % len_ru]
-            else:
-                new_txt += letters_en[(letters_en.find(i) + step * letters_en.find(gen_str[j - spase_couner])) % len_en]
-        cipher.write(new_txt)
+        return 'No such file'
+    with open(args[3], 'w') as cipher:
+        spase_couner = 0
+        for line in usr_file.readlines():
+            new_txt = ''
+            if i == ' ':
+                spase_couner += 1
+            for j, i in enumerate(line.lower()):
+                if i not in letters_en and i not in letters_ru:
+                    new_txt += i
+                elif i in letters_ru:
+                    new_txt += letters_ru[(letters_ru.find(i) + step * letters_en.find(gen_str[j - spase_couner])) % len_ru]
+                else:
+                    new_txt += letters_en[(letters_en.find(i) + step * letters_en.find(gen_str[j - spase_couner])) % len_en]
+            cipher.write(new_txt)
     usr_file.close()
-    cipher.close()
-    return "Result in " + args[3]
+    return 'Result in ' + args[3]
 
 
 def caesar_crack(args):
     '''автовзлом шифра Цезаря(частотный анализ)'''
     global letters_en
     global letters_ru
-    file = open(args[0], "r")
-    ammount = Counter()
-    for line in file.readlines():
-        ammount += Counter(line)
-    file.close()
+    with open(args[0], 'r') as file:
+        ammount = Counter()
+        for line in file.readlines():
+            ammount += Counter(line)
     most_c = ammount.most_common(1)[0][0]
     if most_c == ' ':
         most_c = ammount.most_common(2)[1][0]
@@ -137,8 +136,8 @@ def caesar_crack(args):
     elif most_c in letters_en:
         step = letters_en.find(most_c) - letters_en.find('e')
     else:
-        return "Too many special characters"
-    return caesar([args[0], "de", step, args[1]])
+        return 'Too many special characters'
+    return caesar([args[0], 'de', step, args[1]])
 
 
 def res(ch):
--- graph.py
+++ graph.py
@@ -1,122 +1,134 @@
+import string
 import tkinter
-from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import ttk
-'''Только набросок'''
 
+letters = string.ascii_lowercase
+letters_len = len(letters)
+
+
+# Шифр Цезаря(шифровка/расшифровка)
 def to_Caesar_en():
     usage = combo1.get().lower()
     strk = txt1.get(1.0, tkinter.END).lower()
     try:
         stepp = int(step1.get())
     except:
-        tkinter.messagebox.showerror("Error", "Wrong input")
+        tkinter.messagebox.showerror('Error', 'Wrong input')
         return
-    if usage == "Расшифровать":
+    if usage == 'Расшифровать':
         stepp *= -1
-    dec = 'abcdefghijklmnopqrstuvwxyz'
-    len_d = len(dec)
+    global letters
+    global letters_len
     new_strk = ''
     for i in strk:
-        if i not in dec:
+        if i not in letters:
             new_strk += i
         else:
-            new_strk += dec[(dec.find(i) + stepp) % len_d]
-    tkinter.messagebox.showinfo("Result", new_strk)
+            new_strk += letters[(letters.find(i) + stepp) % letters_len]
+    tkinter.messagebox.showinfo('Result', new_strk)
 
 
+# Шифр Виженера(шифровка/расшифровка)
 def to_Vigenere_en():
     usage = combo2.get().lower()
     strk = txt2.get(1.0, tkinter.END).lower()
     keyy = (step2.get()).lower()
     flag = 1
     if not keyy.isalpha():
-        tkinter.messagebox.showerror("Error", "Wrong input")
+        tkinter.messagebox.showerror('Error', 'Wrong input')
         return
     len_k = len(keyy)
-    if usage == "Расшифровать":
+    if usage == 'Расшифровать':
         flag *= -1
-    dec = 'abcdefghijklmnopqrstuvwxyz'
-    len_d = len(dec)
+    global letters
+    global letters_len
     new_strk = ''
     for j, i in enumerate(strk):
-        if i not in dec:
+        if i not in letters:
             new_strk += i
         else:
-            new_strk += dec[(dec.find(i) + flag * dec.find(keyy[j % len_k])) % len_d]
-    tkinter.messagebox.showinfo("Result", new_strk)
+            new_strk += letters[(letters.find(i) + flag * letters.find(keyy[j % len_k])) % letters_len]
+    tkinter.messagebox.showinfo('Result', new_strk)
 
 
+# Шифр Вернама(шифровка/расшифровка)
 def to_Vernam_en():
     usage = combo3.get().lower()
     strk = txt3.get(1.0, tkinter.END).lower()
     keyy = step3.get().lower()
     flag = 1
     if len(keyy) != len(strk) - 1:
-        tkinter.messagebox.showerror("Error", "Wrong input")
+        tkinter.messagebox.showerror('Error', 'Wrong input')
         return
     len_k = len(keyy)
-    if usage == "Расшифровать":
+    if usage == 'Расшифровать':
         flag *= -1
-    dec = 'abcdefghijklmnopqrstuvwxyz'
-    len_d = len(dec)
+    global letters
+    global letters_len
     new_strk = ''
     for j, i in enumerate(strk):
-        if i not in dec:
+        if i not in letters:
             new_strk += i
         else:
-            new_strk += dec[(dec.find(i) + flag * dec.find(keyy[j])) % len_d]
-    tkinter.messagebox.showinfo("Result", new_strk)
+            new_strk += letters[(letters.find(i) + flag * letters.find(keyy[j])) % letters_len]
+    tkinter.messagebox.showinfo('Result', new_strk)
 
 
+# Создание окна
 window = tkinter.Tk()
-window.title("Недоделано")
+window.title('Ciphers')
 window.geometry('950x600')
+window.resizable(False, False)
 
-name1 = tkinter.Label(window, text="Шифр Цезаря")
+# Блок с шифром Цезаря
+name1 = tkinter.Label(window, text='Шифр Цезаря')
 name1.grid(column=0, row=0, columnspan=2)
-combo1 = ttk.Combobox(window, text="Зашифровать", font=("Arial Bold", 20))
-combo1['values'] = ("Зашифровать", "Расшифровать")
+combo1 = ttk.Combobox(window, text='Зашифровать', font=('Arial Bold', 20))
+combo1['values'] = ('Зашифровать', 'Расшифровать')
 combo1.current(0)
 combo1.grid(column=0, row=1, columnspan=2)
 txt1 = tkinter.Text(window, width=50, height=10, wrap=tkinter.WORD)
 txt1.grid(column=0, row=2, columnspan=2)
-lbl1 = tkinter.Label(window, text="с шагом", font=("Arial bold", 20))
+lbl1 = tkinter.Label(window, text='с шагом', font=('Arial bold', 20))
 lbl1.grid(column=0, row=3)
 step1 = tkinter.Entry(window, width=10)
 step1.grid(column=1, row=3)
-btn1 = tkinter.Button(window, text="Начать", command=to_Caesar_en)
+btn1 = tkinter.Button(window, text='Начать', command=to_Caesar_en)
 btn1.grid(column=0, row=4)
 
+# Разделитель между блоками
 whitespace1 = tkinter.Label(window, text='                          ')
 whitespace1.grid(column=3, row=0, rowspan=4, columnspan=4)
 
-name2 = tkinter.Label(window, text="Шифр Виженера")
+# Блок с шифром Виженера
+name2 = tkinter.Label(window, text='Шифр Виженера')
 name2.grid(column=7, row=0, columnspan=2)
-combo2 = ttk.Combobox(window, text="Зашифровать", font=("Arial Bold", 20))
-combo2['values'] = ("Зашифровать", "Расшифровать")
+combo2 = ttk.Combobox(window, text='Зашифровать', font=('Arial Bold', 20))
+combo2['values'] = ('Зашифровать', 'Расшифровать')
 combo2.current(0)
 combo2.grid(column=7, row=1, columnspan=2)
 txt2 = tkinter.Text(window, width=50, height=10, wrap=tkinter.WORD)
 txt2.grid(column=7, row=2, columnspan=2)
-lbl2 = tkinter.Label(window, text="с ключом", font=("Arial bold", 20))
+lbl2 = tkinter.Label(window, text='с ключом', font=('Arial bold', 20))
 lbl2.grid(column=7, row=3)
 step2 = tkinter.Entry(window, width=20)
 step2.grid(column=8, row=3)
-btn2 = tkinter.Button(window, text="Начать", command=to_Vigenere_en)
+btn2 = tkinter.Button(window, text='Начать', command=to_Vigenere_en)
 btn2.grid(column=7, row=4)
 
-name3 = tkinter.Label(window, text="Шифр Вернама")
+# Блок с шифром Вернама
+name3 = tkinter.Label(window, text='Шифр Вернама')
 name3.grid(column=0, row=6, columnspan=2)
-combo3 = ttk.Combobox(window, text="Зашифровать", font=("Arial Bold", 20))
-combo3['values'] = ("Зашифровать", "Расшифровать")
+combo3 = ttk.Combobox(window, text='Зашифровать', font=('Arial Bold', 20))
+combo3['values'] = ('Зашифровать', 'Расшифровать')
 combo3.current(0)
 combo3.grid(column=0, row=7, columnspan=2)
 txt3 = tkinter.Text(window, width=50, height=10, wrap=tkinter.WORD)
 txt3.grid(column=0, row=8, columnspan=2)
-lbl3 = tkinter.Label(window, text="с кодом", font=("Arial bold", 20))
+lbl3 = tkinter.Label(window, text='с кодом', font=('Arial bold', 20))
 lbl3.grid(column=0, row=9)
 step3 = tkinter.Entry(window, width=10)
 step3.grid(column=1, row=9)
-btn3 = tkinter.Button(window, text="Начать", command=to_Vernam_en)
+btn3 = tkinter.Button(window, text='Начать', command=to_Vernam_en)
 btn3.grid(column=0, row=10)
